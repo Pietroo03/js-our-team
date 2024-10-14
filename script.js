fecbbb594cedb8b255a2cd240c39815146c41108
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-14 22:33:16 +0200

    add changes
    add option for adding a new member

--- script.js
+++ script.js
@@ -37,15 +37,30 @@ const teamMembers = [
   }
 ];
 
+const teamEl = document.getElementById('team')
 const rowEl = document.querySelector('.row')
 
+
+
 for (let i = 0; i < teamMembers.length; i++) {
-    const member = teamMembers[i];
-    console.log(member);
-    let {name, role, email, img} = member
-    let markup = `
-    <div class="col-sm-12 col-md-5 col-lg-3 d-flex bg-dark p-0 m-2">
-      <img src="${img}" alt="" width="80">                         
+  const member = teamMembers[i];
+  console.log(member);
+  
+  const markup = generateMemberCard(member)
+  console.log(markup);
+  
+  
+  teamEl.insertAdjacentHTML('beforeend', markup)
+}
+
+
+function generateMemberCard(member) {
+
+  const {name, role, email, img} = member
+
+  return `
+    <div class="col-12 col-md-5 col-lg-3 d-flex bg-dark p-0">
+      <img src="${img}" alt="" width="100">                         
       <div class="ps-2">
         <h5 class="text-white pt-2"><strong>${name}</strong></h5>
         <div class="text-white">${role}</div>
@@ -53,5 +68,38 @@ for (let i = 0; i < teamMembers.length; i++) {
       </div>
     </div>
     `
-    rowEl.innerHTML += markup
 }
+
+
+const formEl = document.querySelector('form')
+const buttonEl = document.querySelector('button')
+
+formEl.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  let name = document.getElementById('inputName').value
+  let role = document.getElementById('inputRole').value
+  let email = document.getElementById('inputEmail').value
+  let img = document.getElementById('inputImg').value
+
+  const newMember = {
+    name,
+    role,
+    email,
+    img
+  }
+  console.log(newMember);
+  
+
+  const markup = generateMemberCard(newMember)
+  console.log(markup);
+  
+
+  teamEl.insertAdjacentHTML('beforeend', markup)
+  
+
+}) 
+
+
+
+
